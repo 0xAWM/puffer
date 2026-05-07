a5a7349f9b81c59d81cbad0cbbe6d826df9b9258
0xAWM <29773064+0xAWM@users.noreply.github.com>
2026-05-07 13:15:31 +0800

    restore build.rs

--- crates/puffer-runner-grpc/build.rs
+++ crates/puffer-runner-grpc/build.rs
@@ -4,7 +4,6 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     tonic_build::configure()
         .build_client(true)
         .build_server(true)
-        .protoc_arg("--experimental_allow_proto3_optional")
         .compile_protos(&[proto], &["proto"])?;
     Ok(())
 }
